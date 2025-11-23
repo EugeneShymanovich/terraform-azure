8d4cd8bd81db739c4de9e7af84569d597aff62df
Yauheni Shymanovich <39487286+EugeneShymanovich@users.noreply.github.com>
2025-11-23 11:01:49 +0300

    New changes and branch

---
+++  providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e10b0631-fc46-4ab7-878e-2852bbda236f"
+}
